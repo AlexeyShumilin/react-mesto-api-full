31da4b86b42dbf5431cc8bf22f7804c780753492
Alexey Shumilin <shumil.alexey@gmail.com>
2021-02-28 14:48:13 +0200

    middleware. auth
    fixed

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,25 +1,32 @@
 const jwt = require('jsonwebtoken');
+const UnauthorizedErr = require('../errors/unauthorized-err');
 
-module.exports = (req, res, next) => {
+const {
+  NODE_ENV,
+  JWT_SECRET,
+} = process.env;
+
+const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Authorization required ' });
+    return res.status(422).send({error: 'user already exist'});
+
   }
 
   const token = authorization.replace('Bearer ', '');
   let payload;
-
   try {
-    payload = jwt.verify(token, 'dev-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Authorization required ' });
+    res.status(422).send({error: 'user already exist'})
+
   }
 
   req.user = payload;
   next();
+
+  return true;
 };
+
+module.exports = { auth };
